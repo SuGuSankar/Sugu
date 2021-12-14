8cda2092b6cbd7431b4acf11d3a6e115489e9f2c
SuGuSankar <sugumar05sugu@gmail.com>
2021-12-14 17:26:26 +0530

    third

--- src/test/java/org/fblogin/BassClass.java
+++ src/test/java/org/fblogin/BassClass.java
@@ -27,7 +27,7 @@ public class BassClass {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-
+		System.out.println("testB");
 	}
 
 	public static void browserLaunch(String url) {
